81e57b73a6c2bb925eef6f6eda02c10ef20471b7
emresorkun <emresorkun@gmail.com>
2021-01-27 06:40:36 +0100

    min post input tests added,SECOND TEST  IS WORKING

--- cypress/integration/examples/testheader.js
+++ cypress/integration/examples/testheader.js
@@ -1,9 +1,10 @@
 describe('headingTest', ()=>{
     it('contains the correct title', ()=>{
         cy.visit("http://localhost:5000/guestbook");
-        
+
         cy.get("h1")
         .invoke('text')
         .should('equal', 'Gästbok!');
     });
 });
+
